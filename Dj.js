89391d31f0bac73a56ae2d4056444bf038b9973b
Dunyadj <dunya.jadallah18@gmail.com>
2024-05-20 21:14:07 -0400

    Update Dj.js

--- Dj.js
+++ Dj.js
@@ -11,11 +11,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 
 // Add a marker to the map for New York City
-var luigismarker = L.marker([40.68965691085725, -73.96537769031497]).addTo(map);
+var luigismarker = L.marker([40.62489701416209, -74.03062021863167]).addTo(map);
 
 // Add a popup to the marker
 luigismarker.bindPopup(`
-    <b>Luigi's Pizza</b><br>Best pizza on Dekalb Ave<br>
+    <b>Luigi's Pizza</b><br>Best Arabic Food in Bay Ridge <br>
     <img src="https://aurashktest.github.io/aurashktest/images/luigis.png" style="width: 100px; height: auto;">
 `).openPopup();
 
